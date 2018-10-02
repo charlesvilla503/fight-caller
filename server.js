eb5e8e19994fc1081132d66fc1bf2c0bf37966f6
Charles Villa <charlesvilla@outlook.com>
2018-10-02 13:38:07 -0700

    re-organized some functions

--- server.js
+++ server.js
@@ -65,19 +65,6 @@ app.post('/matches', (req, res) => {
 });
 
 
-app.delete('/matches/:id', (req, res) => {
-  FightSched
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).json({ message: 'success' });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
-    });
-});
-
-
 app.put('/matches/:id', (req, res) => {
   if (!(req.params.id && req.body.id === req.body.id)) {
     res.status(400).json({
@@ -99,6 +86,20 @@ app.put('/matches/:id', (req, res) => {
     .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
+app.delete('/matches/:id', (req, res) => {
+  FightSched
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted fight record with id \`${req.params.id}\``);
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+
 
 app.delete('/:id', (req, res) => {
   FightSched
@@ -114,12 +115,8 @@ app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
 });
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
 let server;
 
-// this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
@@ -138,8 +135,6 @@ function runServer(databaseUrl, port = PORT) {
   });
 }
 
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
@@ -154,8 +149,6 @@ function closeServer() {
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
