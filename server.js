d457b55b6fdcb409f2eb24df2f37960f2f4e048c
Charles Villa <charlesvilla@outlook.com>
2018-10-09 12:11:22 -0700

    restructured for Fights router

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 mongoose.Promise = global.Promise;
 
 const { DATABASE_URL, PORT } = require('./config');
-const { FightSched } = require('./models');
+const { router: fightRouter } = require('./fights');
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
@@ -32,6 +32,8 @@ passport.use(jwtStrategy);
 
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
+app.use('/api/fights/', fightRouter);
+
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
@@ -40,106 +42,6 @@ app.get('/api/protected', jwtAuth, (req, res) => {
     data: 'oh we good'
   });
 });
-
-
-app.get('/matches', (req, res) => {
-  FightSched
-    .find()
-    .then(fights => {
-      res.json(fights.map(fight => fight.serialize()));
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
-    });
-});
-
-app.get('/matches/:id', (req, res) => {
-  FightSched
-    .findById(req.params.id)
-    .then(fight => res.json(fight.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went horribly awry' });
-    });
-});
-
-app.post('/matches', (req, res) => {
-  const requiredFields = ['date', 'fighterA', 'fighterB'];
-  for (let i = 0; i < requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  FightSched
-    .create({
-      date: req.body.date,
-      fighterA: req.body.fighterA,
-      fighterABoxrecID: req.body.fighterABoxrecID,
-      fighterB: req.body.fighterB,
-      fighterBBoxrecID: req.body.fighterBBoxrecID,
-      location: req.body.location,
-      television: req.body.television,
-      comments: req.body.televison
-    })
-    .then(fightSched => res.status(201).json(fightSched.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Something went wrong' });
-    });
-
-});
-
-
-app.put('/matches/:id', (req, res) => {
-  if (!(req.params.id && req.body.id === req.body.id)) {
-    res.status(400).json({
-      error: 'Request path id and request body id values must match'
-    });
-  }
-
-  const updated = {};
-  const updateableFields = ['date', 'fighterA', 'fighterB', 'fighterABoxrecID', 'fighterBBoxrecID', 'location', 'television'];
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updated[field] = req.body[field];
-    }
-  });
-
-  FightSched
-    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-    .then(updatedFight => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
-});
-
-app.delete('/matches/:id', (req, res) => {
-  FightSched
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      console.log(`Deleted fight record with id \`${req.params.id}\``);
-      res.status(204).json({ message: 'success' });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
-    });
-});
-
-
-
-app.delete('/:id', (req, res) => {
-  FightSched
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      console.log(`Deleted fight record with id \`${req.params.id}\``);
-      res.status(204).end();
-    });
-});
-
 
 app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
