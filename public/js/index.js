3936f03dfe9e31de620e49c823bfb7c12849bb8f
Charles Villa <charlesvilla@outlook.com>
2018-10-09 13:50:54 -0700

    MVP account creation

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+function submitNew() {
+    $('.newAccount').submit((event) => {
+        event.preventDefault();
+        const user = newUser.value;
+        const pass = newPass.value;
+        const first = firstName.value;
+        const last = lastName.value;
+        const data = {
+            username: `${user}`, password: `${pass}`, firstName: `${first}`, lastName: `${last}`,
+        };
+        $.ajax({
+            url: 'http://localhost:8080/api/users/',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(data),
+            success: (response) => {
+                console.log("User created")
+            },
+
+            error: (err) => {
+                console.log(err);
+            },
+
+        });
+    });
+}
+
+$(submitNew);
